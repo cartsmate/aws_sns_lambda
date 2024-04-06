40f6c3a6c61ed6c474693eaf87604681d574c10b
Andy Carter <andycarter@Andys-MacBook-Pro.local>
2024-04-06 16:14:25 +0100

    remove dead code

--- lambda_function.py
+++ lambda_function.py
@@ -2,43 +2,81 @@ import boto3
 import json
 import requests
 import datetime
-
 from bs4 import BeautifulSoup
-
-from config import Config
-from mail_sender import EmailSender
-from text_sender import TextSender
 
 
 def lambda_handler(event, context):
-    # TEST DEPLOYMENT - with NO DEPENDENCIES OR CREDENTIALS
+
+    now = datetime.datetime.now()
+    print(f"LOGGING: lambda START at: {now}")
+
+    website = 'https://glastonbury.seetickets.com/content/extras'
+    r = requests.get(website)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    finder_status = True
+    search_phrase = 'balance'
+    xs = soup.find_all('p')
+    for idx, content in enumerate(xs):
+        if search_phrase in str(content):
+            break
+        else:
+            finder_status = False
+
+    now = datetime.datetime.now()
+
+    if finder_status:
+        print(f"LOGGING: {now} | keyword: '{search_phrase}' found on site")
+        subject = "no news is bad news"
+        body = f"still sold out: {website}"
+        send_email(subject, body)
+        send_text(f"{subject} {body}")
+    else:
+        print(f"LOGGING: {now} | keyword: '{search_phrase}' NOT found on site .... send warning text message")
+        subject = "ACTION REQUIRED: check glasto website"
+        body = f"quick get on line to get glasto tickets: {website}"
+        send_email(subject, body)
+        send_text(f"{subject} {body}")
+
+    now = datetime.datetime.now()
+    print(f"LOGGING: lambda END at: {now}")
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-    #
-    # r = requests.get('https://glastonbury.seetickets.com/content/extras')
-    # soup = BeautifulSoup(r.content, 'html.parser')
-    #
-    # finder_status = True
-    # search_phrase = 'balance'
-    # xs = soup.find_all('p')
-    # for idx, content in enumerate(xs):
-    #     if search_phrase in str(content):
-    #         break
-    #     else:
-    #         finder_status = False
-    #
-    # now = datetime.datetime.now()
-    # if finder_status:
-    #     print(f"{now} | keyword: '{search_phrase}' found on site")
-    #     EmailSender().send_email_noaction()
-    # else:
-    #     config = Config.get_config()
-    #     print(f"{now} | keyword: '{search_phrase}' NOT found on site .... send warning text message")
-    #     TextSender.send_text(config)
-    #     EmailSender().send_email_action()
 
 
-# if __name__ == '__main__':
-#     print(lambda_handler(event=None, context=None))
+def send_email(subject, body):
+    print('LOGGING: Sending EMAIL process START')
+    client = boto3.client('ses')
+    to_address = 'andyjcarter77@gmail.com'
+    from_address = 'andy@thepubcrawls.co.uk'
+
+    response = client.send_email(
+        Source=from_address,
+        Destination={'ToAddresses': [to_address]},
+        Message={'Subject': {'Data': subject},
+                 'Body': {'Text': {'Data': body}}
+                 }
+    )
+    print(f'LOGGING: {response}')
+    print(f'LOGGING: {subject} email sent')
+    print('LOGGING: Sending EMAIL process END')
+
+
+def send_text(msg):
+    print('LOGGING: Sending TEXT process START')
+    contact_tel = '+447507777568'
+    client = boto3.client('sns')
+    response = client.publish(PhoneNumber=contact_tel,
+                   Message=msg,
+                   MessageAttributes={
+                        'AWS.SNS.SMS.SenderID': {
+                            'DataType': 'String',
+                            'StringValue': 'GlastoAlert'
+                        }}
+                   )
+    print(f'LOGGING: {response}')
+    print(f'LOGGING: {msg} text sent')
+    print('LOGGING: Sending TEXT process END')
